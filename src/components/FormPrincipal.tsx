87850428c2f8b8d7bf49a9f423befbb684742a2d
jesus Gutierrez <jesuslgutierrez685@gmail.com>
2025-05-28 21:03:17 -0600

    creamos la logica del formulari principal y agregamos la libreria uuid para crear id randoms

--- src/components/FormPrincipal.tsx
+++ src/components/FormPrincipal.tsx
@@ -1,19 +1,88 @@
-import React from 'react'
+import { useState } from "react"
+import {v4 as uuidv4} from 'uuid'
 
 const FormPrincipal = () => {
+    const [titulo, setTitulo] = useState('')
+    const [valorGrafico, setValorGrafico] = useState(0)
+    const [fecha, setFecha] = useState(new Date())
+    const [tipoGrafico, setTipoGrafico] = useState("")
+    const tiposDeGraficos = ['line', 'area', 'bar', 'heatmap']
+
+    const handleSubmit = (e) => {
+
+        //flijo de la fecha ennel formulario
+        const fechaFormateada = fecha.toISOString().split('T')[0];
+        //crear un nuevo grafico 
+        const nuevoGrafico = {
+            id: uuidv4(),
+            titulo,
+            tipo: tipoGrafico,
+            series: [{
+                data: [{
+                    x: fechaFormateada,
+                    y: valorGrafico
+                }]
+            }]
+        }
+        console.log('Datos del grafico', nuevoGrafico);
+        e.preventDefault();
+        resertForm()
+    }
+
+
+    const resertForm = () => {
+        setTitulo(titulo)
+        setValorGrafico(valorGrafico)
+        setFecha(fecha)
+        setTipoGrafico(tipoGrafico)
+    }
+
+
     return (
         <>
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <div className="flex justify-around">
-                    <input type="text" name="" placeholder='Nombre del Grafico' className='w-96 m-5 p-1 rounded shadow-lg border border-slate-300 focus:shadow-slate-400 focu:outline-none' id="" />
-                    <input type="number" placeholder='Dato' name="" id="" className='w-24 m-5 p-1 rounded shadow-lg border border-slate-300 focus:shadow-slate-400 focu:outline-none' />
-                    <input type="date" name="" id="" className='w-44 m-5 p-1 rounded shadow-lg border border-slate-300 focus:shadow-slate-400 focu:outline-none' />
-                    <select name="" id="" className='w-48 m-5 p-1 rounded shadow-lg border border-slate-300 focus:shadow-slate-400 focu:outline-none'>
-                        <option value="">Tipo de Grafico</option>
+                    {/* TITULO */}
+                    <input
+                        type="text"
+                        placeholder='Nombre del Grafico'
+                        className='w-96 m-5 p-1 rounded shadow-lg border border-slate-300 focus:shadow-slate-400 focu:outline-none'
+                        value={titulo}
+                        onChange={(e) => setTitulo(e.target.value)}
+                        required
+                       
+                    />
+                    {/* NUMERO */}
+                    <input
+                        type="number"
+                        placeholder='Dato'
+                        className='w-24 m-5 p-1 rounded shadow-lg border border-slate-300 focus:shadow-slate-400 focu:outline-none'
+                        value={valorGrafico}
+                        onChange={(e) => setValorGrafico(Number(e.target.value))}
+                    />
+                    {/* dia */}
+                    <input
+                        type="date"
+                        className='w-44 m-5 p-1 rounded shadow-lg border border-slate-300 focus:shadow-slate-400 focu:outline-none'
+                        value={fecha.toISOString().split('T')[0]}
+                        onChange={(e) => { setFecha(new Date(e.target.value)) }}
+                    />
+                    <select
+                        className='w-48 m-5 p-1 rounded shadow-lg border border-slate-300 focus:shadow-slate-400 focu:outline-none'
+                        value={tipoGrafico}
+                        onChange={(e) => setTipoGrafico(e.target.value)}
+                    >
+                        <option >-- Selecciona un Grafico --</option>
+                        {tiposDeGraficos.map((tipo, index) => (
+                            <option
+                                key={index}
+                                value={tipo}
+                            >{tipo ? tipo : 'No hay nada'}</option>
+                        ))}
                     </select>
                     <button className='bg-blue-600 hover:bg-blue-500 rounded  m-5 p-2 text-white hover:cursor-pointer shadow-lg' type='submit'>Crear Grafico</button>
                 </div>
-                
+
             </form>
         </>
     )
